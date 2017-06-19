6383a871f2ea7eb3251013c3a10dae5920e0887c
naeohmi <naeohmi@gmail.com>
2017-06-19 00:06:14 -0400

    animations and tweaks

--- ef-react/src/components/FourOhFour.js
+++ ef-react/src/components/FourOhFour.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 class FourOhFour extends Component {
 
     render() {
         return (
-            <div className="four-oh-four-wrapper">
-                <h1>Sorry we can't find that page right now!</h1>
-
-                <iframe title="404" src="https://giphy.com/embed/n5ZJhC4NxnDlm" width="480" height="467" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <div className="end-landing-wrapper">
+                <Jumbotron>
+                <h1>Sorry we can't find that page right now!</h1><br /><br />
+       <iframe title="404" src="https://giphy.com/embed/n5ZJhC4NxnDlm" width="480" height="467" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <br />
+            <br />
+                   <div className="start-btn-div">
+                        <ul className="start-btn-ul">
+                            <li className="start-btn-li">
+                                <NavLink to="/play/action">
+                                    <p>click to play again!</p>
+                            </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                    </Jumbotron>
             </div>
         );
     }
--- ef-react/src/components/StartLanding.js
+++ ef-react/src/components/StartLanding.js
@@ -13,22 +13,31 @@ class StartLanding extends Component {
     render() {
         return (
             <div className="start-landing-wrapper">
-                
+
                 <h1 className="title">welcome to:</h1>
-                <a className="main-logo"href="/"><img src="/images/logo.png" alt="logo" /></a>
+                {/*<a className="main-logo"href="/"><img src="/images/logo.png" alt="logo" /></a>*/}
                 <div className="start-info">
 
+                    <div className=" animated jello emo-title"><img src="/images/E0.png" alt="emoticon" /></div>
+                    <div className=" animated flip emo-title"><img src="/images/E1.png" alt="emoticon" /></div>
+                    <div className=" animated tada emo-title"><img src="/images/E2.png" alt="emoticon" /></div>
+                    <div className=" animated pulse emo-title"><img src="/images/E5.png" alt="emoticon" /></div>
+                    <div className=" animated emo-title"><img src="/images/E4.png" alt="emoticon" /></div>
+                    <div className=" animated rotateIn emo-title"><img src="/images/E3.png" alt="emoticon" /></div>
+
+                    <h1 className="title">emotiface</h1>
+                    <br />
                     <div className="start-btn-div">
                         <ul className="start-btn-ul">
                             <li className="start-btn-li">
                                 <NavLink to="/play/action">
                                     <p>click to start!</p>
-                            </NavLink>
+                                </NavLink>
                             </li>
                         </ul>
                     </div>
                 </div>
-                
+
                 <Footer />
             </div>
         );
